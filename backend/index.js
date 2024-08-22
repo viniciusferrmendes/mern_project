f79ae13cef29af3f118e7243a8626a2301737503
Vinicius Mendes <vinicius.ferrmendes@gmail.com>
2024-08-22 19:56:27 +0100

    finalizing middlewares

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,24 @@ const express = require("express");
 const { logger } = require("./middleware/logger.js");
 const errorHandler = require("./middleware/errorHandler.js");
 const path = require("path");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions.js");
 
 const PORT = process.env.PORT || 3500;
 
 const app = express();
 
 app.use(logger);
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
+
+app.use(cookieParser());
+
 app.use("/", express.static(path.join(__dirname, "public")));
+
 app.use("/", require("./routes/root.js"));
 
 app.all("*", (req, res) => {
@@ -22,5 +32,7 @@ app.all("*", (req, res) => {
         res.type("txt").send("404 - Not Found")
     }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
