bb1090f1e31cd2ebb709e3c8762b8a79c9d7a19e
Vinicius Mendes <vinicius.ferrmendes@gmail.com>
2024-08-24 11:23:03 +0100

    configuring mongodb/mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,19 @@
+require("dotenv").config();
 const express = require("express");
-const { logger } = require("./middleware/logger.js");
-const errorHandler = require("./middleware/errorHandler.js");
+const app = express();
 const path = require("path");
+const errorHandler = require("./middleware/errorHandler.js");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions.js");
-
+const connectDB = require("./config/dbConnection.js");
+const mongoose = require("mongoose");
+const { logger, logEvents } = require("./middleware/logger.js");
 const PORT = process.env.PORT || 3500;
 
-const app = express();
+console.log(process.env.NODE_ENV);
+
+connectDB();
 
 app.use(logger);
 
@@ -35,4 +40,12 @@ app.all("*", (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+mongoose.connection.once("open", () => {
+    console.log("Connected to MongoDB.");
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+});
+
+mongoose.connection.on("error", error => {
+    console.log(error);
+    logEvents(`${error.no}: ${error.code}\t${error.syscall}\t${error.hostname}`, 'mongoErrorLog.log');
+});
